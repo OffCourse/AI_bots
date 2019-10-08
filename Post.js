9671b289fbaabfde0793ce0abb657a3b7c4abf57
Scott <schepers.scott@hotmail.com>
2019-10-08 13:19:15 +0200

    Added: get all data function Changed: create data is now a function

--- Post.js
+++ Post.js
@@ -1,25 +1,49 @@
-const Axios  = require("axios");
+const Axios = require("axios");
 
-var config = {
-    headers: { "Authorization": "Bearer " + "fnADaLYJAlACC3T4--QFqHBrS-L3tb5MFP0_U_q9" }
+var events = require('events');
+var emitter = new events.EventEmitter;
+
+function postDB(query) {
+    Axios({
+        url: 'https://graphql.fauna.com/graphql',
+        method: 'post',
+        headers: { "Authorization": "Bearer " + "fnADaLYJAlACC3T4--QFqHBrS-L3tb5MFP0_U_q9" },
+        data: {
+            query: query
+        }
+    }).then((result) => {
+        console.log(result.data)
+    });
 }
 
-Axios({
-    url: 'https://graphql.fauna.com/graphql',
-    method: 'post',
-    headers: { "Authorization": "Bearer " + "fnADaLYJAlACC3T4--QFqHBrS-L3tb5MFP0_U_q9" },
-    data: {
-        query: `       
-            mutation createPosts {
-            createPosts(data: { id: 3, tags: ["HUH"], text: "#HUH" }) { 
-                id 
-                tags 
-                text
-                } 
-            }
-  
-        `
-    }
-}).then((result) => {
-    console.log(result.data)
-});
+function getAllData() {
+    Axios({
+        url: 'https://graphql.fauna.com/graphql',
+        method: 'post',
+        headers: { "Authorization": "Bearer " + "fnADaLYJAlACC3T4--QFqHBrS-L3tb5MFP0_U_q9" },
+        data: {
+            query: `    
+                query FindAllPosts {
+                    allPosts {
+                        data {
+                            id
+                            tags
+                            text
+                    }
+                }
+            }`
+        }
+    }).then((result) => {
+
+        // console.log(result.data.data.allPosts.data);
+        emitter.emit('loaded', result.data.data.allPosts.data)
+    });
+}
+
+getAllData();
+
+emitter.on('loaded', (data) => {
+    console.log(data);
+})
+
+//postDB('mutation createPosts {createPosts(data: { id: 4, tags: ["Method"], text: "het is gelukt via een #Method" }) {    id     tags     text    } }')
