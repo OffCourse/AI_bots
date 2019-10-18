8de19ae84b333e719e4352303e7ea06feaf286d7
Peter Sabel (Zabel) <peter@sabelzabel.nl>
2019-10-18 16:09:10 +0200

    scraping works with hardcoded credentials

--- Puppeteer.js
+++ Puppeteer.js
@@ -1,29 +1,26 @@
 const puppeteer = require('puppeteer');
-const Logic = require('./Logic')
 
-async function scrollToBottom( page, itemTargetCount = 0, scrollDelay = 0) {
-    var number_of_scrolls_needed = itemTargetCount / 12;
-    try {
-        let previousHeight;
-        for (i = 0; i < number_of_scrolls_needed; i++) {
-            previousHeight = await page.evaluate('document.body.scrollHeight');
-            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-            await page.waitFor(scrollDelay);    
-            console.log("scrolled", i, "times.")        
-        }
-    } catch(e) { 
-        console.log(e)
-    }
+async function scrollToBottom(page, n_posts, scrollDelay = 0) {
+    returnArray = [];
+    do {
+        previousHeight = await page.evaluate('document.body.scrollHeight');
+        await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+        await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+        await page.waitFor(scrollDelay);
+        returnArray = await scapeAndAdd(page, returnArray);
+        console.log(returnArray.length)
+    } while (returnArray.length < n_posts);
+    return returnArray;
 }
 
-async function scrape(page) {
-    await page.waitForSelector('.v1Nh3.kIKUG._bz0w');
-    return result = await page.evaluate(() => {
-        const elements = document.querySelectorAll('.v1Nh3.kIKUG._bz0w a');
-        const linksArr = Array.from(elements).map(link => link.href);
-        return linksArr;
-    });
+async function scapeAndAdd(page, urlList){
+    results = await page.$$eval('.v1Nh3.kIKUG._bz0w a', links => links.map(link => link.href));
+    for(i in results) {
+        if(!urlList.includes(results[i])) {
+            urlList.push(results[i]);
+        }
+    }
+    return urlList;
 }
 
 async function login(page) {
@@ -58,16 +55,10 @@ exports.follow = async(username, password) => {
     spanElement = spanElement.pop();
     spanElement = await spanElement.getProperty('innerText');
     spanElement = await spanElement.jsonValue();
-    await scrollToBottom(page, 111);
-    const results = await page.$$eval('.v1Nh3.kIKUG._bz0w a', links => links.map(link => link.href))
-
-    for(url in results){
-        console.log(results[url])
-    }
+    urlArray = await scrollToBottom(page, spanElement);
 
     console.log("ready to die now");
     console.log("please kill me")
-
 
     // // Close the browser.
     // await browser.close();
