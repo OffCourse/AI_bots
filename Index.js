8799b2528d3203a7dfae9f2b285bc55affa46605
Scott <schepers.scott@hotmail.com>
2019-10-29 19:00:19 +0100

    Added: now the most used words through all the posts of a user will be shown

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const instaCasette = require("./InstaCasette");
 
 const insta = new instaCasette(process.env.USERNAME_IG, process.env.PASSWORD_IG);
 
-insta.getUrlList("onskoningshuis").then(data => console.log(data)).catch(error => console.log(error));
+insta.getUrlList("markvanderneut").then(data => console.log("Data = " +data)).catch(error => console.log(error));
 
 //insta.getUser().then(data => console.log(data)).catch(error => console.log(error));
 
--- Puppeteer.js
+++ Puppeteer.js
@@ -1,7 +1,10 @@
 const puppeteer = require("puppeteer");
 
+var name;
+
 async function scrollToBottom(page, n_posts, scrollDelay = 1000) {
 	n_posts = n_posts.replace(",", "");
+
 	var returnArray = [];
 	do {
 		var previousHeight = await page.evaluate("document.body.scrollHeight");
@@ -24,7 +27,7 @@ async function scrapeUrls(page, urlList, totalPosts){
 			urlList.push(results[index]);
 		}
 	}
-	console.log("scraped " + urlList.length + " of the " + totalPosts + " posts.");
+	console.log("scraped " + urlList.length + " of the " + totalPosts + " post URLs");
 	return urlList;
 }
 
@@ -45,13 +48,21 @@ async function login(page) {
 }
 
 async function getValue(page, tag, property) {
-	var tmp = await page.$(tag);
-	tmp = await tmp.getProperty(property);
-	return await tmp.jsonValue();
+	try {
+		var tmp = await page.$(tag);
+		tmp = await tmp.getProperty(property);
+		return await tmp.jsonValue();
+	} catch (error) {
+		return;
+	}
+
 }
 
 async function scrapePost(page, urlArray) {
+	var postList = [];
+	var i = 0;
 	for(var index in urlArray) {
+		
 		await page.goto(urlArray[index]);
 		// try{
 		// 	await page.$eval((".glyphsSpriteCircle_add__outline__24__grey_9.u-__7"));
@@ -61,6 +72,10 @@ async function scrapePost(page, urlArray) {
 		// }
 
 		var post = await getValue(page, ".C4VMK", "innerText");
+		try{
+			post = PostCleanup(post);
+			postList.push(post);
+		} catch (error) {}
 
 		var responses = await page.$$(".Mr508");
 
@@ -79,19 +94,57 @@ async function scrapePost(page, urlArray) {
 			likes = await getValue(page, ".vcOH2", "innerText");
 		}
 
-		console.log("\nUrl:                  " + urlArray[index]);
-		console.log("Post date:            " + postdate);
-		console.log("Location:             " + location);
-		console.log("Ammount of responses: " + responses.length);
-		console.log("Ammount of likes:     " + likes);
-		console.log("Post text:\n" + post);
+		// console.log("\nUrl:                  " + urlArray[index]);
+		// console.log("Post date:            " + postdate);
+		// console.log("Location:             " + location);
+		// console.log("Ammount of responses: " + responses.length);
+		// console.log("Ammount of likes:     " + likes);
+		// console.log("Post text:\n" + post);
+		console.log("scraped " + (i += 1) + " of the " + urlArray.length + " posts");
 	}
+	console.log(postList);
+	CountWords(postList);
+}
+
+//Todo: Add file with discluded words
+function PostCleanup(post){
+	post = post.toLowerCase();
+	post = post.replace(/\n/g, " ");
+	post = post.replace(name, "");
+	//post = post.replace(/[0-9]/g, "");	
+	post = post.replace(/[^[a-z0-9]]/g, "");
+	post = post.replace(/ [a-z] /g, "");
+	post = post.replace(/ de | het | een | van | op | in | is | en | met | voor | zijn | day /g, " ");
+	post = post.replace(/ +/, " ");
+	return post;
+}
+
+function CountWords(list){
+	let countList = [];
+	let splitList = [];
+	list.forEach(el => {
+		splitList.push(el.match(/\b(\w+)\b/g));
+	});
+
+	splitList.forEach(el => {
+		el.forEach(element => {
+			countList[element] = (countList[element] || 0) + 1;
+		});
+	});
+
+	var sorted = Object.keys(countList).sort( function(a,b) {
+		return countList[b] - countList[a];
+	});
+
+	console.log(sorted);
+	console.log(name + " is tagged with: " + sorted[0] + ", " + sorted[1] + ", " + sorted[2] + ", " + sorted[3] + ", " + sorted[4]);
 }
 
 exports.follow = async(username) => {
 	const browser = await puppeteer.launch({
 		headless: true,
 	});
+	name = username;
 	const page = await browser.newPage();
 	page.setViewport({ width: 1280, height: 926 });
     
@@ -100,6 +153,7 @@ exports.follow = async(username) => {
 	await page.goto(`https://www.instagram.com/${username}/`);
 	var ammount_of_posts = await getValue(page, ".Y8-fY", "innerText");
 	ammount_of_posts = ammount_of_posts.replace(" posts", "");
+
 	var urlArray = await scrollToBottom(page, ammount_of_posts);
 	await scrapePost(page, urlArray);
 
