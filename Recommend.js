54e7dd3bc0a1a63b75032affb589fbf7f71f9789
MarcoBergsma <marcobergsma1999@gmail.com>
2019-11-14 11:27:11 +0100

    Moved functionality
    Moved the functionality from the html page to a seperate javascript file

--- Recommend.js
+++ Recommend.js
@@ -1,7 +1,18 @@
-function display() {
-	console.log("Spaghetti and meatballs");
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
 }
 
-document.getElementById("textarea").addEventListener("input", function() {
-	console.log("Je moeder is een hamster");
-});
+function onChange() {
+	const text = document.getElementById("textarea").value;
+	var start = "";
+	if (text.split(" ").length > 5) {
+		const amountOfTags = document.getElementById("amountOfTags").value
+		const hashtags = ["#Blessed", "#Satan", "#BeachLife", "#DyingRightNow", "#666", "#NotTheDevil", "#Idk", "#GoingHome", "#BoycotScott"];
+		for (var i = 0; i < amountOfTags; i++) {
+			const index = getRandomInt(hashtags.length);
+			start += " " + hashtags[index];
+		}
+	}
+
+	document.getElementById("result").innerHTML = text + start;
+}
