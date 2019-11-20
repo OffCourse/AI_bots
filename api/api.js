74075cc3f0f34117824b9fe8b50aa1e6426f9a40
Peter Sabel (Zabel) <peter@sabelzabel.nl>
2019-11-20 12:31:09 +0100

    working async api

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-const fs = require("fs");
+const recommend = require("../back/Recommend");
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/getRecommend/:username/:tag", (req, res) => {
-	getRecommendations(req.params.username ,req.params.tag).then(function(result) {
+	recommend.getRecommendations(req.params.username, req.params.tag).then(function (result) {
 		return res.send(result);
 	});
 });
@@ -29,141 +29,6 @@ app.delete("/", (req, res) => {
 	return res.send("Received a DELETE HTTP method");
 });
 
-app.listen(process.env.PORT, () =>
-	console.log(`Example app listening on port ${process.env.PORT}!`),
+app.listen(process.env.API_PORT, () =>
+	console.log(`Example app listening on port ${process.env.API_PORT}!`),
 );
-
-async function prepareData(rawData) {
-	var dataList = [];
-	var count = 0;
-
-	const encodedDictionary = createDictionary(rawData);
-	rawData.forEach(string => {
-		var words = splitString(string);
-		words.forEach(function (word, wordIndex) {
-			var values = [];
-			var amountOfAdjacentWords = 1; //Amount of adjacent words to look for, in front of and behind the corresponding word
-			for (var adjacentIndex = -amountOfAdjacentWords; adjacentIndex <= amountOfAdjacentWords; adjacentIndex++) {
-				if (adjacentIndex != 0 && words[wordIndex + adjacentIndex] != undefined) {
-					var wordInDictionary = encodedDictionary.filter(function (element) {
-						return element.label === words[wordIndex + adjacentIndex];
-					});
-					values.push(wordInDictionary[0].label); //Note: change "label" to "value" when using K-means 
-				}
-			}
-
-			if (dataList.some(element => element["label"] === word)) {
-				var wordInDataList = dataList.filter(function (element) {
-					return element.label === word;
-				});
-				var oldVector = dataList[wordInDataList[0].index].vector;
-				var newVector = oldVector.concat(values);
-				dataList[wordInDataList[0].index].vector = newVector;
-			}
-			else {
-				dataList.push({ label: word, vector: values, index: count++ });
-			}
-		});
-	});
-	return dataList;
-}
-
-function prepareTarget(preparedData, targetLabel) {
-	console.log("Data is: " + preparedData);
-	var target = {};
-	if (preparedData.some(element => element["label"] === targetLabel)) {
-		var targetInDataList = preparedData.filter(function (element) {
-			return element.label === targetLabel;
-		});
-		target = { label: targetLabel, vector: targetInDataList[0].vector };
-	} else {
-		target = { label: targetLabel, vector: preparedData.length };
-	}
-	return target;
-}
-
-async function getUserData(username) {
-	var userData;
-	const TweetItModule = require("../back/TweetIt");
-	const fs = require("fs");
-	const TweetIt = new TweetItModule();
-	TweetIt.getText(username).then(function (result) {
-		userData = prepareData(result);
-		fs.writeFile(`${username}_data.json`, userData, console.log);
-		return userData;
-	});
-}
-
-async function getRecommendations(username, targetLabel) {
-	var preparedData;
-	let preparedTarget;
-	let recommendations;
-	try {
-		preparedData = require(`../${username}_data.json`);
-		// eslint-disable-next-line no-empty
-		preparedTarget = prepareTarget(preparedData, targetLabel);
-		recommendations = evaluate(preparedData, preparedTarget);
-	} catch (error) { }
-
-	if (preparedData == null) {
-		await getUserData(username).then(function(result){
-			preparedData = result;
-			preparedTarget = prepareTarget(preparedData, targetLabel);
-			recommendations = evaluate(preparedData, preparedTarget);
-		});
-	}
-	
-	return recommendations;
-
-
-	//const rawData = require("../cleaned_tweets.json");
-	//const preparedData = prepareData(rawData);
-	//const preparedData = require("../prepared_data.json");
-
-	//targetLabel = targetLabel.toLowerCase();
-	//const preparedTarget = prepareTarget(preparedData, targetLabel);
-	//const recommendations = evaluate(preparedData, preparedTarget);
-	//return recommendations;
-}
-
-function evaluate(data, target) {
-	const Logic = require("../back/Logic");
-	var recommendations = [];
-	if (data.some(element => element["label"] === target.label)) {
-		var targetInData = data.filter(function (element) {
-			return element.label === target.label;
-		});
-		targetInData[0].vector.forEach(word => {
-			if (word !== target.label) recommendations.push(word);
-		});
-	} else {
-		// //If target is not in dataset, return random recommendations
-		// var randomIndex = Math.floor(Math.random() * data.length);
-		// data[randomIndex].vector.forEach(word => {
-		// 	if (word !== target.label) recommendations.push(word);
-		// });
-	}
-	recommendations = Logic.countWords(recommendations);
-	return recommendations;
-}
-
-function createDictionary(data) {
-	var dict = [];
-	var count = 0;
-	data.forEach(string => {
-		var words = splitString(string);
-		words.forEach(word => {
-			if (!dict.some(element => element["label"] === word)) {
-				dict.push({ label: word, value: count++ });
-			}
-		});
-	});
-	return dict;
-}
-
-function splitString(string) {
-	var words = string.split(/\s+/);
-	// eslint-disable-next-line quotes
-	words = words.filter(word => word != ''); //Remove empty strings
-	return words;
-}
